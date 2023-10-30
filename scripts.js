0a5adae461d8ef112440a71e93da727d90cf4e06
AdamTaylor9 <tayloradamross@gmail.com>
2023-10-30 02:34:03 -0500

    Javascript for newsletter section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const input = document.getElementById('email');
+    const message_res = document.querySelector('.message');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const email = input.value.trim();
+
+        if (email === '') {
+        message_res.textContent = 'Please enter a valid email address.';
+        } else {
+        message_res.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    });
+});
+
